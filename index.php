f1bcc496ae900d589e32b794506dac1545711acd
Saad Shaikh <saadsk9288@gmail.com>
2022-10-25 00:23:08 +0530

    add

--- index.php
+++ index.php
@@ -14,26 +14,35 @@
     echo "Hello Welcome to $Name's Third programming Language Webpage, Yeah PHP!!!! <br/><br/> ";
     echo " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <br/><br/> ";
     print ("Saad Hello <br/><br/>");
-    print ('saaad' .'saad');
-    $Username = "Saad";
-    $password = 12345 ; 
+    print ('saaad' .'saad'. '<br/><br/>');
+//     $Username = "Saad";
+//     $password = 12345 ; 
 
-    if ($Username === "Saad"){
-        Print ("Welcome Saad");
-    }
-    else{
-        Print("Please Try Again");
-    };
-for($i=0; $i<100;$i++){
-    if($i == 42)
-    // {break;
-    // };
-    // It will print numbers till 41 
-    {continue;
-    };
-    // It will print numbers from 1 to 99 except 42
+//     if ($Username === "Saad"){
+//         Print ("Welcome Saad");
+//     }
+//     else{
+//         Print("Please Try Again");
+//     };
+// for($i=0; $i<100;$i++){
+//     if($i == 42)
+//     // {break;
+//     // };
+//     // It will print numbers till 41 
+//     {continue;
+//     };
+//     // It will print numbers from 1 to 99 except 42
 
-    echo "$i <br/>";
+//     echo "$i <br/>";
+// };
+
+$counter = 0;
+while($counter < 10){
+    echo "This is value printed in while loop as $counter <br/><br/>";
+    $counter++;
+};
+for($another=0; $another<10; $another++){
+echo "This is the value printed inside for loop as $another <br/><br/> ";
 };
     ?>
 </body>
