12f3beaacb66238abec267a166dffe8d9ebdd309
Saad Shaikh <saadsk9288@gmail.com>
2022-10-24 23:58:21 +0530

    adding second commit

--- index.php
+++ index.php
@@ -9,8 +9,20 @@
 <body>
     <?php 
     $Name = "Shaikh Mohammad Saad";
-    echo "Hello Welcome to $Name's Third programming Language Webpage, Yeah PHP!!!! ";
-    
+    echo "Hello Welcome to $Name's Third programming Language Webpage, Yeah PHP!!!! <br/> ";
+    echo " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <br/> ";
+    print ("Saad Hello <br/>");
+    print ('saaad' .'saad')
+    $Username = "Saad";
+    $password = 12345 ; 
+
+    if ($Username === "Saad"){
+        Print ("Welcome Saad");
+    }
+    else{
+        Print("Please Try Again");
+    };
+
     ?>
 </body>
 </html>
