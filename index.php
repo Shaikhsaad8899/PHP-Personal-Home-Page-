06459ba884b81634d2410bd9bf740e9704d0c5ca
Saad Shaikh <saadsk9288@gmail.com>
2022-10-25 22:26:15 +0530

    add

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 
 <body>
     <?php 
-    // $Name = "Shaikh Mohammad Saad";
-    // echo "Hello Welcome to $Name's Third programming Language Webpage, Yeah PHP!!!! <br/><br/> ";
+    $Name = "Shaikh Mohammad Saad";
+    echo "Hello Welcome to $Name's Third programming Language Webpage, Yeah PHP!!!! <br/><br/> ";
     // echo " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <br/><br/> ";
     // print ("Saad Hello <br/><br/>");
     // print ('saaad' .'saad'. '<br/><br/>');
@@ -38,37 +38,52 @@
 
 // $counter = 1;
 // while($counter <=  5){
-    echo "This is value printed in while loop as $counter <br/><br/>";
-    $counter++;
-};
-for($another=1; $another<= 5; $another++){
-    $abc=$another*$another;
-echo "This is the value printed inside for loop as $abc <br/><br/> " ;
-};
-$Array = array(1,2,3,4,5);
-foreach($Array as $num){
-    echo "This is the value printed inside forEach loop as $num <br/><br/> ";
-};
-$counters = 1;
-while($counters <=  5){
-    $efg = $counters*$counters*$counters ;
-    echo "Cube for $counters is $efg <br/><br/>";
-    $counters++;
-};
-for($another=1; $another<= 20; $another++){
-    $abc=$another*$another;
-echo "Square of $another is $abc <br/><br/> " ;
-};
-$Arrayname = array("Sam"=> "123","Salim"=> "1234","Raj"=> "1235"  );
-forEach($Arrayname as $EFG){
-    if ($Arrayname["Sam"] == "123"){
-        echo "welcome Sam <br/>";
-    }
-    elseif ($Arrayname === "Salim"){
-        echo "Please Try Again";
-    }
+//     echo "This is value printed in while loop as $counter <br/><br/>";
+//     $counter++;
+// };
+// for($another=1; $another<= 5; $another++){
+//     $abc=$another*$another;
+// echo "This is the value printed inside for loop as $abc <br/><br/> " ;
+// };
+// $Array = array(1,2,3,4,5);
+// foreach($Array as $num){
+//     echo "This is the value printed inside forEach loop as $num <br/><br/> ";
+// };
+// $counters = 1;
+// while($counters <=  5){
+//     $efg = $counters*$counters*$counters ;
+//     echo "Cube for $counters is $efg <br/><br/>";
+//     $counters++;
+// };
+// for($another=1; $another<= 20; $another++){
+//     $abc=$another*$another;
+// echo "Square of $another is $abc <br/><br/> " ;
+// };
+// $Arrayname = array("Sam"=> "123","Salim"=> "1234","Raj"=> "1235"  );
+// forEach($Arrayname as $EFG){
+//     if ($Arrayname["Sam"] == "123"){
+//         echo "welcome Sam <br/>";
+//     }
+//     elseif ($Arrayname === "Salim"){
+//         echo "Please Try Again";
+//     }
    
-};
+// };
+
+// $bdate = '23';
+// $bmonth = '04';
+// $byear = '2000';
+
+// $bday = juliantojd($bdate,$bmonth,$byear);
+// echo "Your Birthday is on the " . jddayofweek($bday, 1);
+// This gives the day of your birth
+
+
+// $name = "saad.com";
+// $ipAddr = gethostbyname($name);
+// echo "this is the IP address of $name: $ipAddr ";
+
+
     ?>
 </body>
 
